bde3e214b233e623d6dc6c7b4d43503ae9980314
Daryn Oden <daryn.oden@gmail.com>
2026-03-05 09:40:12 -0800

    Update purgenvim.sh

--- apps/.local/bin/purgenvim.sh
+++ apps/.local/bin/purgenvim.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+clear
+
+echo "WARNING-WARNING-WARNING-WARNING"
+echo "This script will purge nvim..."
+echo "YOU HAVE BEEN WARNED!!"
+read -p "Continue? (y/n) " ans
+
+# echo $ans
+
+if [[ $ans == "y" ]]
+then
+  echo "Purging..."
+  rm -rf ~/.config/nvim
+  rm -rf ~/.local/state/nvim
+  rm -rf ~/.local/share/nvim
+else
+  echo "Good Call!"
+fi
